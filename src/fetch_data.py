4d9377f4f73bf03bf54daa78b8b7c66e77a8079c
Pawannikam378 <pawannikam378@gmail.com>
2026-04-09 14:37:39 +0530

    Implement data fetching and storage for Bollywood tracks; add access token management

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -1,28 +1,57 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
-import time 
+import time
 
-time.sleep(1)  # To avoid hitting rate limits
+CLIENT_ID = "aea1a425b3d64a90b404f07fb06a143b"
+CLIENT_SECRET = "153c1d3df441488b803a321969e44b1d"
 
+auth_manager = SpotifyClientCredentials(
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET
+)
 
-CLIENT_ID = "ID"
-CLIENT_SECRET = ""
+sp = spotipy.Spotify(
+    auth_manager=auth_manager,
+    requests_timeout=10,
+    retries=3
+)
 
-def fetch_tracks(query="year:2023", limit=50):
-    auth = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
-    sp = spotipy.Spotipy(auth_manager=auth)
-    sp.search(q='bollywood', type='track', limit=50)
-    result = sp.search(q=query, type='track', limit=limit)
-    tracks = result['tracks']['items']
+queries = [
+    "bollywood", "arijit singh"
+]
 
-    track_ids = [track['id'] for track in tracks]
-    features = sp.audio_features(track_ids)
+def fetch_data():
+    all_data = []
 
-    df = pd.DataFrame(features)
-    return df
+    for query in queries:
+        print(f"Fetching: {query}")
+        try: 
+            results = sp.search(q=query, type='track', limit=1, market="IN")
+            tracks = results['tracks']['items']
+            track_ids = [track['id'] for track in tracks]
+            features_list = sp.audio_features(track_ids)
+            
+            for i, track in enumerate(tracks):
+                features = features_list[i]
 
+                if features is None:
+                    continue
+
+                features['popularity'] = track['popularity']
+                features['track_name'] = track['name']
+                features['artist'] = track['artists'][0]['name']
+
+                all_data.append(features)
+
+            time.sleep(0.5)
+
+        except Exception as e:
+            print("Error:", e)
+
+    df = pd.DataFrame(all_data)
+    df.to_csv("data/spotify_data.csv", index=False)
+    print("Done!")
 
 if __name__ == "__main__":
-    df = fetch_tracks()
-    df.to_csv("spotify_tracks.csv", index=False)
+    fetch_data()
