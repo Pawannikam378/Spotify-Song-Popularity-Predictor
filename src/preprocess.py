1fd6f6675f62d9115d959048029f630131bf80ba
Pawannikam378 <pawannikam378@gmail.com>
2026-04-09 14:57:34 +0530

    Enhance preprocessing function: update data source and add new features
    - Changed the CSV file path to the new location for loading data.
    - Added new feature 'energy_dance' as a product of 'energy' and 'danceability'.
    - Introduced 'tempo_bucket' to categorize 'tempo' into bins.
    - Updated feature selection to include relevant columns for model training.

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,11 +1,25 @@
-import pandas as pd 
+import pandas as pd
 
 def preprocess():
-    df - pd.read_csv("data/raw.csv")
+    df = pd.read_csv("E:\D Drive Data\Project\Spotify Song Prediction System\Spotify-Song-Popularity-Predictor\data\spotify_data.csv")
+
     df = df.dropna()
     df = df.drop_duplicates()
+    df['energy_dance'] = df['energy'] * df['danceability']
+    df['tempo_bucket'] = pd.cut(df['tempo'], bins=[0, 90, 120, 200], labels=[0,1,2])
+    features = [
+        'danceability',
+        'energy',
+        'loudness',
+        'speechiness',
+        'acousticness',
+        'instrumentalness',
+        'liveness',
+        'valence',
+        'tempo'
+    ]
 
-    X = df.drop(columns=['popularity'])
+    X = df[features]
     y = df['popularity']
 
     return X, y
